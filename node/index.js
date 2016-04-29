4c5e4655e1dfa8e3b8bb723f592e9018e76f641f
Eramik <erami.ukr@gmail.com>
2016-04-29 15:51:49 +0300

    CSS changed

--- node/index.js
+++ node/index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-    res.sendfile('index.html');
+    res.sendfile('index2.html');
 });
 var usersOnline = 0;
 var usersCount = 0;
